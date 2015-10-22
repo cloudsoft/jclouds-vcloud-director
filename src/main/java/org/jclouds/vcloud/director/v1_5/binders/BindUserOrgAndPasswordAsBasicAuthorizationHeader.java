3161a0f5bb8c2b7911bb68b7ef9393d3a824afff
Andrea Turli <andrea.turli@gmail.com>
2015-10-22 13:11:59 +0200

    remove useless ACCEPT_ENCODING and CONTENT_ENCODING from BindUserOrgAndPasswordAsBasicAuthorizationHeader

--- src/main/java/org/jclouds/vcloud/director/v1_5/binders/BindUserOrgAndPasswordAsBasicAuthorizationHeader.java
+++ src/main/java/org/jclouds/vcloud/director/v1_5/binders/BindUserOrgAndPasswordAsBasicAuthorizationHeader.java
@@ -46,8 +46,6 @@ public class BindUserOrgAndPasswordAsBasicAuthorizationHeader implements MapBind
                         checkNotNull(postParams.get("org"), "org"),
                         checkNotNull(postParams.get("password"), "password")).getBytes(UTF_8));
       return (R) request.toBuilder()
-              .replaceHeader(HttpHeaders.ACCEPT_ENCODING, "gzip")
-              .replaceHeader(HttpHeaders.CONTENT_ENCODING, "gzip")
               .replaceHeader(HttpHeaders.ACCEPT, "application/*+xml;version=1.5")
               .replaceHeader(HttpHeaders.AUTHORIZATION, header).build();
    }
